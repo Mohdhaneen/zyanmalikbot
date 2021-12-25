dbd42a92b7dd3c53c6bf1c4d42181dce139a9417
Mohd haneen <90704922+Mohdhaneen@users.noreply.github.com>
2021-12-25 18:05:43 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -16,26 +16,26 @@ Basic Commads
 /start :- check am Alive
 
 Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
+☣️ /broadcast Replay Any Message or Media
+☣️ /stats user Status
+☣️ /ban_user  click ban_user more info
+☣️ /unban_user click unban_user more info
+☣️ /banned_users Banned User Details
+☣️ /total How Many Files Added In Database
+☣️ /logger  Get Logs
+☣️ /delete Delete File From Database
 
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+☣️ Name :  ZYAN MALIK 
     
-➥ Creator : [Naughty PROFESSOR](
+☣️ Creator : [Naughty PROFESSOR](
 https://youtube.com/c/NaughtyPROFESSOR)
 
-➥ Language : Python3
+☣️ Language : Python3
 
-➥ Library : Pyrogram Asyncio 1.13.0
+☣️ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+☣️ MAIN CHANNEL : [**MAIN CHANNEL**](https://t.me/cinemacenterofficial)
 """
